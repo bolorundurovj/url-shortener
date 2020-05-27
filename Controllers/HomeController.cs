830ad240b21183521c553170acf4713bc26642d2
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-27 13:51:45 +0100

    Finished Up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,27 +5,79 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver;
+using  shortid;
 using url_shortener.Models;
 
 namespace url_shortener.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMongoDatabase mongoDatabase;
+        private const string ServiceUrl = "http://localhost:5000";
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            
+            var connectionString = "mongodb://localhost:27017/";
+            var mongoClient = new MongoClient(connectionString);
+            mongoDatabase = mongoClient.GetDatabase("url-shortener");
         }
-
-        public IActionResult Index()
+        
+        [HttpGet]
+        public async Task<IActionResult> Index(string u)
         {
-            return View();
+            // get shortened url collection
+            var shortenedUrlCollection = mongoDatabase.GetCollection<ShortenedUrl>("shortened-urls");
+            // first check if we have the short code
+            var shortenedUrl = await shortenedUrlCollection
+                .AsQueryable()
+                .FirstOrDefaultAsync(x => x.ShortCode == u);
+
+            // if the short code does not exist, send back to home page
+            if (shortenedUrl == null)
+            {
+                return View();
+            }
+
+            return Redirect(shortenedUrl.OriginalUrl);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> ShortenUrl(string longUrl)
+        {
+            // get shortened url collection
+            var shortenedUrlCollection = mongoDatabase.GetCollection<ShortenedUrl>("shortened-urls");
+            // first check if we have the url stored
+            var shortenedUrl = await shortenedUrlCollection
+                .AsQueryable()
+                .FirstOrDefaultAsync(x => x.OriginalUrl == longUrl);
+
+            // if the long url has not been shortened
+            if (shortenedUrl == null)
+            {
+                var shortCode = ShortId.Generate(length: 8);
+                shortenedUrl = new ShortenedUrl
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    OriginalUrl = longUrl,
+                    ShortCode = shortCode,
+                    ShortUrl = $"{ServiceUrl}/{shortCode}"
+                };
+                // add to database
+                await shortenedUrlCollection.InsertOneAsync(shortenedUrl);
+            }
+
+            return View(shortenedUrl);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/ShortenedUrl.cs
@@ -0,0 +1,16 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace url_shortener.Models
+{
+    public class ShortenedUrl
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortCode { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
